dd35e2152b7a567a75b2c4adc1ceb74b05f53d88
Irushinie <Irushiniedilmika@gmail.com>
2021-06-18 13:52:26 +0530

    Security oAuth code flow implementation - video 12 commit

---
+++ Security oAuth auth code flow implementation-video 12/rent-ui/src/main/java/lk/irushinie/rentcloud/rentui/RentUiApplication.java
@@ -0,0 +1,17 @@
+package lk.irushinie.rentcloud.rentui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentUiApplication.class, args);
+	}
+
+}
