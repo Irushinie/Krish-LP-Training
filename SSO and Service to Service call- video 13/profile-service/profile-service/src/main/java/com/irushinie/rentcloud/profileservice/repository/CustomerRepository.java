8865506457110d5e43c29b145ae9191d16ba5e29
Irushinie <Irushiniedilmika@gmail.com>
2021-06-18 17:18:40 +0530

    SSO and Service to Service call- video 13 commit

---
+++ SSO and Service to Service call- video 13/profile-service/profile-service/src/main/java/com/irushinie/rentcloud/profileservice/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.irushinie.rentcloud.profileservice.repository;
+
+import com.irushinie.rentcloud.commons.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface CustomerRepository extends JpaRepository<Customer,Integer> {
+
+}
